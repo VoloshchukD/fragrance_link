1ab582418372bd4c96d1574741dad8ff8251dffb
Daniil <voloshchukd7@gmail.com>
2025-01-22 21:35:21 +0400

    Initial commit

---
+++ src/main/java/com/aroma/fragrance_link/FragranceLinkApplication.java
@@ -0,0 +1,13 @@
+package com.aroma.fragrance_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragranceLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragranceLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aroma/fragrance_link/FragranceLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroma.fragrance_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragranceLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
