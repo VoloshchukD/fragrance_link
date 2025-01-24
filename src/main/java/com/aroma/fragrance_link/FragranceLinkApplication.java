eaf61bf0a2c46d360721c4a7c73f992eafa37fc2
Daniil <voloshchukd7@gmail.com>
2025-01-24 11:02:00 +0400

    Add base react ui

--- src/main/java/com/aroma/fragrance_link/FragranceLinkApplication.java
+++ src/main/java/com/aroma/fragrance_link/FragranceLinkApplication.java
@@ -2,12 +2,14 @@ package com.aroma.fragrance_link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class FragranceLinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FragranceLinkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FragranceLinkApplication.class, args);
+    }
 
 }
