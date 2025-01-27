4aea71104b9ca5af9b6994a203823542f4f96dfa
Daniil <voloshchukd7@gmail.com>
2025-01-27 20:34:44 +0400

    Add react router pages

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,14 +1,27 @@
-import PerfumeTable from './components/PerfumeTable'
+import "./App.css";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import PerfumeTable from "./components/PerfumeTable";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <div className="App">
-      <PerfumeTable></PerfumeTable>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Layout />}>
+              <Route index element={<Home/>} />
+              <Route path={"perfumes"} element={<PerfumeTable />} />
+              <Route path={"*"} element={<Error/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </QueryClientProvider>
   );
 }
--- src/ui/src/components/PerfumeTable.js
+++ src/ui/src/components/PerfumeTable.js
@@ -1,39 +1,45 @@
 import { useQuery } from "react-query";
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const retrievePerfumes = async () => {
-    const response = await axios.get(
-      "/api/perfumes",
-    );
-    return response.data;
-  };
+  const response = await axios.get("/api/perfumes");
+  return response.data;
+};
 
 function PerfumeTable() {
-
   const { isLoading, data } = useQuery("perfumes", retrievePerfumes);
 
-  console.log('reposne: ' + JSON.stringify(data));
+  console.log("reposne: " + JSON.stringify(data));
 
   const perfumes = data?._embedded?.perfumes || [];
 
   return (
     <>
       <h1>Perfumes</h1>
+      <button type="button" class="btn btn-primary add">Add New</button>
       <table className="table">
         <thead>
           <tr>
             <th scope="col">Brand</th>
             <th scope="col">Name</th>
             <th scope="col">Description</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
           {perfumes.map((perfume) => (
             <tr>
+              <td>###</td>
               <td>{perfume.name}</td>
               <td>{perfume.description}</td>
-            </tr>)
-            )}
+              <td>
+                <button type="button" class="btn btn-warning">Edit</button>
+                <button type="button" class="btn btn-danger delete">Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
